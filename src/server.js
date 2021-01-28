e3e252047738541bb025ea9ce741c87ab5f97da2
Asli <aslinurkeklikci@yahoo.com>
2021-01-28 17:09:06 -0500

    working on controllers for user registration

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+const RegisterController = require('./controllers/RegisterController');
 
 const PORT = process.env.PORT || 8000
 
@@ -9,13 +10,14 @@ if (process.env.NODE_ENV !== 'production') {
 	require('dotenv').config()
 }
 
-
 app.use(cors())
 app.use(express.json())
 
 app.get('/', (req, res) => {
 	res.send('Hello from Nodemon');
 })
+
+app.post('/register', RegisterController.store)
 
 try {
 	mongoose.connect(process.env.MONGO_DB_SECRET, {
@@ -26,7 +28,6 @@ try {
 } catch (error) {
 	console.log(error)
 }
-
 
 app.listen(PORT, () => {
 	console.log(`Listening on ${PORT}`)
