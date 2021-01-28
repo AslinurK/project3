583136f2aefdf6ca0ae924a17f072ddbcb923323
Asli <aslinurkeklikci@yahoo.com>
2021-01-28 17:58:28 -0500

    routes updated

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,23 @@
 const express = require('express')
-const routes = express.Router();
+const multer = require('multer')
+
 const UserController = require('./controllers/UserController')
+const EventController = require('./controllers/EventController')
+const uploadConfig = require('./config/upload')
+
+const routes = express.Router();
+const upload = multer(uploadConfig);
 
 routes.get('/', (req, res) => {
     res.send({ status: 200 })
 })
 
 
+//event
+routes.post('/event')
+
+
+//user
 routes.post('/register', UserController.store)
 routes.get('/user/:userId', UserController.getUserById)
 
