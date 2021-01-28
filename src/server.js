72ea51b8fd947c997ab7e520063d7f331e03554e
Asli <aslinurkeklikci@yahoo.com>
2021-01-28 18:30:23 -0500

    added dashboard controller

--- src/server.js
+++ src/server.js
@@ -2,17 +2,16 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const routes =require('./routes');
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 8000
-
-if (process.env.NODE_ENV !== 'production') {
-	require('dotenv').config()
-}
 
 app.use(cors())
 app.use(express.json())
 
-
+if (process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
 
 try {
 	mongoose.connect(process.env.MONGO_DB_SECRET, {
@@ -24,6 +23,7 @@ try {
 	console.log(error)
 }
 
+app.use("/files", express.static(path.resolve(__dirname, "..", "files")))
 app.use(routes);
 
 app.listen(PORT, () => {
