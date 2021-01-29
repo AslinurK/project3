37496377dba88aef9c829f49ad26260c5055b05a
Asli <aslinurkeklikci@yahoo.com>
2021-01-28 20:12:41 -0500

    dashboard component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 
-        <login/>
+        <Login/>
+        <Dashboard />
 
     </div>
   );
